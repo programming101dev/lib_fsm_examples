a6fb82c2857beb2d3e190d2ce27c0843150d6e28
programming101dev <113087924+programming101dev@users.noreply.github.com>
2025-04-26 11:36:02 -0700

    updated to new build system

---
+++ config.cmake
@@ -0,0 +1,106 @@
+set(PROJECT_NAME "fsm_run")
+set(PROJECT_VERSION "1.0.0")
+set(PROJECT_DESCRIPTION "Finite State Machine - run")
+set(PROJECT_LANGUAGE "C")
+
+set(CMAKE_C_STANDARD 17)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS OFF)
+
+# Define targets
+set(EXECUTABLE_TARGETS
+        create_destroy
+        fsm_run
+        get_name
+        set_get_bad_change_state_handler
+        set_get_bad_change_state_notifier
+        set_get_did_change_state_notifier
+        set_get_will_change_state_notifier
+)
+
+set(LIBRARY_TARGETS "")
+
+set(create_destroy_SOURCES
+        create_destroy/main.c
+)
+
+set(create_destroy_HEADERS "")
+
+set(create_destroy_LINK_LIBRARIES
+        p101_error
+        p101_env
+        p101_fsm
+)
+
+set(fsm_run_SOURCES
+        fsm_run/main.c
+)
+
+set(fsm_run_HEADERS "")
+
+set(fsm_run_LINK_LIBRARIES
+        p101_error
+        p101_env
+        p101_posix
+        p101_fsm
+)
+
+set(get_name_SOURCES
+        get_name/main.c
+)
+
+set(get_name_HEADERS "")
+
+set(get_name_LINK_LIBRARIES
+        p101_error
+        p101_env
+        p101_fsm
+)
+
+set(set_get_bad_change_state_handler_SOURCES
+        create_destroy/main.c
+)
+
+set(set_get_bad_change_state_handler_HEADERS "")
+
+set(set_get_bad_change_state_handler_LINK_LIBRARIES
+        p101_error
+        p101_env
+        p101_fsm
+)
+
+set(set_get_bad_change_state_notifier_SOURCES
+        set_get_bad_change_state_notifier/main.c
+)
+
+set(set_get_bad_change_state_notifier_HEADERS "")
+
+set(set_get_bad_change_state_notifier_LINK_LIBRARIES
+        p101_error
+        p101_env
+        p101_fsm
+)
+
+set(set_get_did_change_state_notifier_SOURCES
+        set_get_did_change_state_notifier/main.c
+)
+
+set(set_get_did_change_state_notifier_HEADERS "")
+
+set(set_get_did_change_state_notifier_LINK_LIBRARIES
+        p101_error
+        p101_env
+        p101_fsm
+)
+
+set(set_get_will_change_state_notifier_SOURCES
+        set_get_will_change_state_notifier/main.c
+)
+
+set(set_get_will_change_state_notifier_HEADERS "")
+
+set(set_get_will_change_state_notifier_LINK_LIBRARIES
+        p101_error
+        p101_env
+        p101_fsm
+)

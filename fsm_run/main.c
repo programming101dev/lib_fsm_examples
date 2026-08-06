98cd207b533c3cae01c4e523cc8f7c2935d2bd2c
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 03:18:09 -0700

    Enforce semantic source contracts

--- fsm_run/main.c
+++ fsm_run/main.c
@@ -1,4 +1,6 @@
-#include <p101_cli/cli.h>
+#include <p101_cli/p101_getopt.h>
+#include <p101_cli/p101_stdlib.h>
+#include <p101_cli/p101_unistd.h>
 #include <p101_fsm/fsm.h>
 #include <stdio.h>
 #include <stdlib.h>
